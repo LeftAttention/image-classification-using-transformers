cefbba216451ba309df2a841ff8abf4eccea9c81
LeftAttention <74410448+LeftAttention@users.noreply.github.com>
2021-03-04 00:12:35 +0530

    Update tnt.py

--- transformer/tnt.py
+++ transformer/tnt.py
@@ -4,3 +4,30 @@ from torch import nn, einsum
 
 from einops import rearrange, repeat
 from einops.layers.torch import Rearrange
+
+def exists(val):
+    return val is not None
+
+# classes
+
+class PreNorm(nn.Module):
+    def __init__(self, dim, fn):
+        super().__init__()
+        self.norm = nn.LayerNorm(dim)
+        self.fn = fn
+
+    def forward(self, x, **kwargs):
+        return self.fn(self.norm(x), **kwargs)
+
+class FeedForward(nn.Module):
+    def __init__(self, dim, mult = 4, dropout = 0.):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(dim, dim * mult),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(dim * mult, dim)
+        )
+
+    def forward(self, x):
+        return self.net(x)
